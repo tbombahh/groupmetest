0469411cfb228751c85c13b9de1a2a0ba47393e0
tbombahh <39538339+tbombahh@users.noreply.github.com>
2018-05-22 17:41:30 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,61 @@
 var HTTPS = require('https');
+var HTTP = require('http');
 var cool2 = require('cool-ascii-faces');
 
 var botID = process.env.BOT_ID;
+var apiKey = process.env.API_KEY;
 
+##giphy code
 function respond() {
+  var request = JSON.parse(this.req.chunks[0]),
+      giphyCommand = '/giphy';
+
+  if(request.text &&
+     request.text.length > giphyCommand.length &&
+     request.text.substring(0, giphyCommand.length) === giphyCommand) {
+    this.res.writeHead(200);
+    searchGiphy(request.text.substring(giphyCommand.length + 1));
+    this.res.end();
+  } else {
+    this.res.writeHead(200);
+    this.res.end();
+  }
+}
+
+function searchGiphy(giphyToSearch) {
+  var options = {
+    host: 'api.giphy.com',
+    path: '/v1/gifs/search?q=' + encodeQuery(giphyToSearch) + '&api_key=' + apiKey
+  };
+
+  var callback = function(response) {
+    var str = '';
+
+    response.on('data', function(chunck){
+      str += chunck;
+    });
+
+    response.on('end', function() {
+      if (!(str && JSON.parse(str).data[0])) {
+        postMessage('Couldn\'t find a gif 💩');
+      } else {
+        var id = JSON.parse(str).data[0].id;
+        var giphyURL = 'http://i.giphy.com/' + id + '.gif';
+        postMessage(giphyURL);
+      }
+    });
+  };
+
+  HTTP.request(options, callback).end();
+}
+
+function encodeQuery(query) {
+  return query.replace(/\s/g, '+');;
+}
+
+
+## Original
+/* function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\/cool guyy$/;
 
@@ -17,7 +69,7 @@ function respond() {
     this.res.end();
   }
 }
-
+*/
 function postMessage() {
   var botResponse, options, body, botReq;
 
