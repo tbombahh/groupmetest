906cce1a1602ffaa1461b3d6f0807cc3d87d12fb
tbombahh <39538339+tbombahh@users.noreply.github.com>
2018-05-22 17:10:42 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 var HTTPS = require('https');
-var cool = require('cool-ascii-faces');
+var cool2 = require('cool-ascii-faces');
 
 var botID = process.env.BOT_ID;
 
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = cool();
+  botResponse = cool2();
 
   options = {
     hostname: 'api.groupme.com',
