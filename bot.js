5ca44fc873333ad356c4a76d06b8872677ae739a
tbombahh <39538339+tbombahh@users.noreply.github.com>
2018-05-23 21:07:21 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,6 +16,7 @@ function respond() {
       triggered = /^\/triggered$/;
       help = /^\/help$/;
       pokemon = /^\/pokemon$/;
+      yn = /^\/yn$/;
 
 var str = request.text;
 // string and removes spaces and repeating
@@ -113,11 +114,20 @@ if(aires)
           postMessage(botResponse);
           this.res.end();
   }    
-	  else if(request.text && pokemon.test(request.text)) {
+        else if(request.text && pokemon.test(request.text)) {
           this.res.writeHead(200);
           var botResponse = "You want to play Pokemon, ehh?  Here's how to play:  Challenge a fellow trainer with an awesome intro in style of the Red, Blue, and Yellow games.  Then type '/g pokemon' to summon your Pokemon Fighter.  The person you challenge will do the same.  Rules are: Pokemon status is taken from the image (ie asleep, confused, etc.).  Type/Element strengths and weaknesses apply.  If there is more than 1 Pokemon in the image the main focus of the image is your choice.  You can redo if there is no clear Pokemon as the main focus of the image.   We will do a quick vote if there is confusion.  Good luck!" 
           postMessage(botResponse);
           this.res.end();
+  }	  
+        else if(request.text && yn.test(request.text)) {
+          this.res.writeHead(200);
+          var botResponse = "coin was flipped!" 
+          postMessage(botResponse);
+          this.res.end();
+  }     
+		  
+		  
   }    else { console.log("don't care");
     this.res.writeHead(200);
     this.res.end();
