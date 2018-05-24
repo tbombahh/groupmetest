199b577248192b5bc188e50e5bb9184c21ee1be0
tbombahh <39538339+tbombahh@users.noreply.github.com>
2018-05-23 21:42:57 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -123,7 +123,7 @@ if(aires)
   }	  
         else if(request.text && yn.test(request.text)) {
           this.res.writeHead(200);
-		var r = 0;
+		var r = Math.random();
 		var theflip = 'heads';
 		if (r < 0.5) {
 			theflip = 'tails';
