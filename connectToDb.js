277dd8ab96d46912c05c58a9da86bc68e3f92533
tbombahh <39538339+tbombahh@users.noreply.github.com>
2018-05-23 20:30:21 -0500

    Delete connectToDb.js

--- connectToDb.js
+++
@@ -1,8 +0,0 @@
-// This will turn a MongoConnect Promise into an Observable using the fromPromise method
-const fromPromise = require('rxjs').Observable.fromPromise,
-	MongoClient = require('mongodb').MongoClient,
-	//Grab the connection string from env variables (if prod), or from config.js (if local)
-	uri = process.env.MONGODB_URI || require('./config').MONGODB_URI;
-
-const connectPromise = MongoClient.connect(uri);
-module.exports = fromPromise(connectPromise);
