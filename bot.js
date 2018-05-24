11e9e3c12210f0e41dc120b2856c6f2bb83bc4ca
tbombahh <39538339+tbombahh@users.noreply.github.com>
2018-05-24 18:43:39 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -269,36 +269,7 @@ function search(query)
 	});
 }
 
-//Post to Groupme
-function postMessage(botResponse)
-{
-	var options, body, botReq;
-	options = {
-		hostname: 'api.groupme.com',
-		path: '/v3/bots/post',
-		method: 'POST'
-	};
-	body = {
-		"bot_id" : botID,
-		"text" : botResponse
-	};
-	console.log('sending ' + botResponse + ' to ' + botID);
-	botReq = HTTPS.request(options, function(res) {
-		if(res.statusCode == 202) {
-        //neat
-		} else {
-			console.log('rejecting bad status code ' + res.statusCode);
-		}
-	});
-	botReq.on('error', function(err) {
-		console.log('error posting message '  + JSON.stringify(err));
-	});
-	botReq.on('timeout', function(err) {
-		console.log('timeout posting message '  + JSON.stringify(err));
-	});
-	botReq.end(JSON.stringify(body));
-}
-
+/Magic 8Ball Code
 function eightBall()
 {
 	var r = Math.floor((Math.random() * 100) + 1);
@@ -364,5 +335,38 @@ function eightBall()
 	var botResponse = magic;
 	postMessage(botResponse);
 }
+
+
+
+//Post to Groupme
+function postMessage(botResponse)
+{
+	var options, body, botReq;
+	options = {
+		hostname: 'api.groupme.com',
+		path: '/v3/bots/post',
+		method: 'POST'
+	};
+	body = {
+		"bot_id" : botID,
+		"text" : botResponse
+	};
+	console.log('sending ' + botResponse + ' to ' + botID);
+	botReq = HTTPS.request(options, function(res) {
+		if(res.statusCode == 202) {
+        //neat
+		} else {
+			console.log('rejecting bad status code ' + res.statusCode);
+		}
+	});
+	botReq.on('error', function(err) {
+		console.log('error posting message '  + JSON.stringify(err));
+	});
+	botReq.on('timeout', function(err) {
+		console.log('timeout posting message '  + JSON.stringify(err));
+	});
+	botReq.end(JSON.stringify(body));
+}
+
 
 exports.respond = respond;
